18a0e7e45d913062a548efc614c40d13bbaed420
Biketova111 <89241781930@mail.ru>
2023-02-27 17:50:04 +0900

    Add task

---
+++ task2/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+using System.Diagnostics.CodeAnalysis;
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+int GetSumNumber(int n, int m, int sum)
+{
+    if (n == m)
+    {
+        sum +=n;
+        return sum;
+    }
+    else
+    {
+        sum+=n;
+        n++;
+        return sum =GetSumNumber(n, m, sum);
+    }
+}
+
+
+int n = GetNumber("Введите число N: ");
+int m = GetNumber("Введите число M: ");
+int sum = GetSumNumber(n, m, 0);
+Console.WriteLine($"{sum }");
---
+++ task3/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+int GetAckermannFunction (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return GetAckermannFunction(m - 1, 1);
+    if (m > 0 && n > 0) return GetAckermannFunction(m - 1, GetAckermannFunction(m, n - 1));
+return GetAckermannFunction(m, n);
+}
+
+
+int m = GetNumber("Введите число M: ");
+int n = GetNumber("Введите число N: ");
+int AckermannFunction = GetAckermannFunction (m, n);
+Console.WriteLine ($"Функция Аккермана А({m}, {n}) = {AckermannFunction}");
